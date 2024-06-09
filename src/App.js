cb555b2b95babfebf53a54ec679f5989a293e500
Mohammad Afreen Aqthar <110152840+md-afreen-aqthar@users.noreply.github.com>
2024-06-09 18:47:14 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react';
+import firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const auth=firebase.auth();
+firebase.initializeApp({
+  apiKey: "AIzaSyDRrC6bJJLD2gSRot69EVYUkgE6wqi0NFE",
+  authDomain: "sign-project-e6d2f.firebaseapp.com",
+  projectId: "sign-project-e6d2f",
+  storageBucket: "sign-project-e6d2f.appspot.com",
+  messagingSenderId: "309878014994",
+  appId: "1:309878014994:web:eaa5ec6313b3307d1344cb"
+})
+const App=()=>{
+  const [user,setUser]=useState(null);
+ useEffect(()=>{
+auth.onAuthStateChanged(person=>{
+  if(person){
+    setUser(person);
+  }
+  else{
+    setUser(null);
+  }
+})
+ },[])
+  const signInWithGoogle = async() => {
+try{
+await auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
 }
+catch(err){
+  console.log(err);
+}
+  }
+  return(
+    <div>
+      <center>
+        {user ? <div>
+<h1>Welcome To Home</h1>
+<button>Sign Out</button>
+        </div>:
+        <button onClick={signInWithGoogle}>Sign In With Google</button>
+}
+      </center>
+    </div>
+  )
 
+}
 export default App;
